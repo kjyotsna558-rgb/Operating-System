8ec214620e3f0c2848aa9216a017c6c5512b3f41
kjyotsna558-rgb <kjyotsna558@gmail.com>
2026-01-22 22:26:04 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,58 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <time.h>
 
 int main() {
-    int arr[30], key1, key2, status;
-    pid_t p1, p2;
-    srand(time(NULL));
-    printf("Array: ");
-    for (int i = 0; i < 30; i++) {
-        arr[i] = rand() % 50 + 1;
-        printf("%d ", arr[i]);
+    int array[] = {1, 2, 3, 4, 5};
+    int n = sizeof(array) / sizeof(array[0]);
+    int sum = 0;
+    float average;
+    int sum_greater = 0;
+    for (int i = 0; i < n; i++) {
+        sum= sum+array[i];
     }
-    printf("\n\nEnter Key 1 and Key 2: ");
-    scanf("%d %d", &key1, &key2);
-
-    // Child 1
-    p1 = fork();
-    if (p1 == 0) {
-        int found = 0;
-        for (int i = 0; i < 30; i++) {
-            if (arr[i] == key1) {
-                found = 1;
-                break; 
-            }
+    average = (float)sum / n;
+    for (int i = 0; i < n; i++) {
+        if (array[i] > average) {
+            
+            sum_greater += array[i];
         }
-        exit(found); 
     }
-
-    waitpid(p1, &status, 0);
-    int res1 = WEXITSTATUS(status);
-
-    // Child 2
-    p2 = fork();
-    if (p2 == 0) {
-        int found = 0;
-        for (int i = 0; i < 30; i++) {
-            if (arr[i] == key2) {
-                found = 1;
-                break; 
-            }
-        }
-        exit(found); 
-    }
-
-    waitpid(p2, &status, 0);
-    int res2 = WEXITSTATUS(status);
-
-    if (p1 > 0 && p2 > 0) {
-        printf("\nParent Process:\n");
-        printf("Key %d was %s found in the array.\n", key1, res1 ? "" : "not");
-        printf("Key %d was %s found in the array.\n", key2, res2 ? "" : "not");
-    }
-
+    printf("The average of the array elements is: %.2f\n", average);
+    printf("sum of elements greater than the average is: %d\n", sum_greater);
     return 0;
 }
